a74a5900c58176445af9af6d54c43c1a5e53a06a
Cecilia <ccchimento@gmail.com>
2024-05-31 14:23:23 -0600

    buttons added

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,7 +1,15 @@
-import parks from "../data/parks"
+import { useState } from "react"
+import parks0 from "../data/parks0"
 
 export default function Table(){
-    const parksDisplay = parks.map(park  => <tr key={park.id}>
+    const [start, setStart] = useState(0)
+    const [sortBy, setSortBy] = useState("name")
+
+    const maxPages = Math.floor(parks0.length/10)
+    console.log(maxPages * 10 - 1)
+
+    const parksDisplay = parks0.slice(start, start + 10)
+    .map(park  => <tr key={park.id}>
         <td>{park.name}</td>
         <td>{park.location}</td>
         <td>{park.founded_year}</td>
@@ -9,20 +17,30 @@ export default function Table(){
         <td>{park.visitors_2022}</td>
     </tr>)
 
+
     return (
-        <table>
+        <>
+        <table id="table">
             <thead>
             <tr>
-                <th>Name</th>
-                <th>Location</th>
-                <th>Founded</th>
-                <th>Acres</th>
-                <th>Visitors in 2022</th>
+                <th>Name <span name="name" onClick={() => setSortBy("name")}>{sortBy === "name" ? "🔼" : "🔽"}</span></th>
+                <th>Location <span value="location" onClick={() => setSortBy("location")}>{sortBy === "location" ? "🔼" : "🔽"}</span></th>
+                <th>Founded <span name="founded" onClick={() => setSortBy("founded")}>{sortBy === "founded" ? "🔼" : "🔽"}</span></th>
+                <th>Acres <span name="acres" onClick={() => setSortBy("acres")}>{sortBy === "acres" ? "🔼" : "🔽"}</span></th>
+                <th>Visitors in 2022 <span name="visitors" onClick={() => setSortBy("visitors")}>{sortBy === "visitors" ? "🔼" : "🔽"}</span></th>
             </tr>
             </thead>
             <tbody>
                 {parksDisplay}
             </tbody>
         </table>
+
+        <div>
+            <button disabled={start === 0} onClick={() => setStart(0)}>First</button>
+            <button disabled={start === 0} onClick={() => setStart(start - 10)}>Previous</button>
+            <button onClick={() => setStart(start + 10)}>Next</button>
+            <button >Last</button>
+        </div>
+        </>
     )
 }
