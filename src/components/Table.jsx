4e792a0d76b6766a68931af4b95505dd65bdeabb
Cecilia <ccchimento@gmail.com>
2024-06-04 21:05:50 -0600

    sort functionality

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,13 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import parks from "../data/parks";
 
 export default function Table() {
   const [start, setStart] = useState(0);
   const [sortBy, setSortBy] = useState("name");
-
+  const [sortedParks, setSortedParks] = useState([]);
   const lastPageStart = Math.floor(parks.length / 10) * 10;
 
-  const parksDisplay = parks.slice(start, start + 10).map((park) => (
+  const tempParks = parks.sort((a, b) => {
+    const valA = a[sortBy];
+    const valB = b[sortBy];
+    if (valA < valB) {
+      return -1;
+    } else if (valA > valB) {
+      return 1;
+    } else return 0;
+  });
+//   setSortedParks(tempParks);
+
+  const parksDisplay = tempParks.slice(start, start + 10).map((park) => (
     <tr key={park.id}>
       <td>{park.name}</td>
       <td>{park.location}</td>
@@ -17,6 +28,10 @@ export default function Table() {
     </tr>
   ));
 
+  //   function sortParks(){
+
+  //   }
+
   return (
     <>
       <table id="table">
@@ -25,31 +40,31 @@ export default function Table() {
             <th>
               Name{" "}
               <span name="name" onClick={() => setSortBy("name")}>
-                {sortBy === "name" ? "🔼" : "🔽"}
+                {sortBy === "name" ? "▲" : "▼"}
               </span>
             </th>
             <th>
               Location{" "}
               <span value="location" onClick={() => setSortBy("location")}>
-                {sortBy === "location" ? "🔼" : "🔽"}
+                {sortBy === "location" ? "▲" : "▼"}
               </span>
             </th>
             <th>
               Founded{" "}
-              <span name="founded" onClick={() => setSortBy("founded")}>
-                {sortBy === "founded" ? "🔼" : "🔽"}
+              <span name="founded" onClick={() => setSortBy("founded_year")}>
+                {sortBy === "founded_year" ? "▲" : "▼"}
               </span>
             </th>
             <th>
               Acres{" "}
-              <span name="acres" onClick={() => setSortBy("acres")}>
-                {sortBy === "acres" ? "🔼" : "🔽"}
+              <span name="acres" onClick={() => setSortBy("area_acres")}>
+                {sortBy === "area_acres" ? "▲" : "▼"}
               </span>
             </th>
             <th>
-              Visitors in 2022{" "}
-              <span name="visitors" onClick={() => setSortBy("visitors")}>
-                {sortBy === "visitors" ? "🔼" : "🔽"}
+              2022 Visitors{" "}
+              <span name="visitors" onClick={() => setSortBy("visitors_2022")}>
+                {sortBy === "visitors_2022" ? "▲" : "▼"}
               </span>
             </th>
           </tr>
@@ -64,8 +79,18 @@ export default function Table() {
         <button disabled={start === 0} onClick={() => setStart(start - 10)}>
           Previous
         </button>
-        <button disabled={start === lastPageStart} onClick={() => setStart(start + 10)}>Next</button>
-        <button disabled={start === lastPageStart} onClick={() => setStart(lastPageStart)}>Last</button>
+        <button
+          disabled={start === lastPageStart}
+          onClick={() => setStart(start + 10)}
+        >
+          Next
+        </button>
+        <button
+          disabled={start === lastPageStart}
+          onClick={() => setStart(lastPageStart)}
+        >
+          Last
+        </button>
       </div>
     </>
   );
